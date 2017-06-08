e94d2a1e3a4c14b45b8b9285fdd8de07651d9de8
zahra abdallah <zkon360@gmail.com>
2017-06-08 01:17:29 -0400

    Saved data in controller

--- blog/app/controllers/thoughts_controller.rb
+++ blog/app/controllers/thoughts_controller.rb
@@ -3,8 +3,17 @@ class ThoughtsController < ApplicationController
 def new
 end
 
+
 def create
-  render plain: params[:thought].inspect
+  @thought = Thought.new(thought_params)
+ 
+  @thought.save
+  redirect_to @thought
 end
+ 
+private
+  def thought_params
+    params.require(:thought).permit(:title, :text)
+  end
 
 end
