61b161e6a54aed240155c0da092e06efe7c076b7
zahra abdallah <zkon360@gmail.com>
2017-06-08 16:53:13 -0400

    Added a Second Model to App

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170608050226) do
+ActiveRecord::Schema.define(version: 20170608202020) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "thought_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["thought_id"], name: "index_comments_on_thought_id"
+  end
 
   create_table "thoughts", force: :cascade do |t|
     t.string "title"
