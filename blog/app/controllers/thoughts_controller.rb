b0bea2a9de085795056efd94c6931cdcdf2a79f7
zahra abdallah <zkon360@gmail.com>
2017-06-08 16:16:36 -0400

    Added Delete functionality. Crud functionality works!

--- blog/app/controllers/thoughts_controller.rb
+++ blog/app/controllers/thoughts_controller.rb
@@ -14,6 +14,10 @@ def new
   @thought = Thought.new
 end
 
+def edit
+  @thought = Thought.find(params[:id])
+end
+
 
 def create
   @thought = Thought.new(thought_params)
@@ -31,6 +35,30 @@ end
   def show
     @thought = Thought.find(params[:id])
   end
+
+def update
+  @thought = Thought.find(params[:id])
+ 
+  if @thought.update(thought_params)
+    redirect_to @thought
+  else
+    render 'edit'
+  end
+end
+
+
+def destroy
+  @thought = Thought.find(params[:id])
+  @thought.destroy
+ 
+  redirect_to thoughts_path
+end
+
+ 
+# private
+#   def article_params
+#     params.require(:article).permit(:title, :text)
+#   end
 
 
 private
