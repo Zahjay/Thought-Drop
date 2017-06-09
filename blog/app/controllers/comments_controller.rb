3a784944726c68d1c9414c49f2492381aba36071
zahra abdallah <zkon360@gmail.com>
2017-06-09 10:17:05 -0400

    Finished crud functionality for comments

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -6,6 +6,13 @@ class CommentsController < ApplicationController
     redirect_to thought_path(@thought)
   end
 
+def destroy
+    @thought = Thought.find(params[:thought_id])
+    @comment = @thought.comments.find(params[:id])
+    @comment.destroy
+    redirect_to thought_path(@thought)
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
